993fad541a8356faaee12e3a6876353c585bd71a
khs <hanseog201@naver.com>
2024-05-16 10:17:35 +0900

    lessonMain으로 연결까지

---
+++ UFC/src/main/java/edu/kh/cooof/lesson/main/controller/LessonController.java
@@ -0,0 +1,22 @@
+package edu.kh.cooof.lesson.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("lesson")
+@RequiredArgsConstructor
+public class LessonController {
+	
+	@GetMapping("") // get 요청은 바로 레슨 메인으로
+	public String lessonMainPage() {
+		
+		return "lesson/lessonMain";
+	}
+
+}
