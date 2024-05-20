65460d48c5af2319f1e00169982383b7cc9e1ab6
inje <vkd2009@naver.com>
2024-05-20 19:52:51 +0900

    결제 api 넣기 성공  (아직 결제 내용 전달안됨 , 제약조건 해야함 )

--- UFC/src/main/java/edu/kh/cooof/gym/trainerSelect/controller/TrainerSelectController.java
+++ UFC/src/main/java/edu/kh/cooof/gym/trainerSelect/controller/TrainerSelectController.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.cooof.gym.trainerSelect.model.dto.Trainer;
 import edu.kh.cooof.gym.trainerSelect.model.service.TrainerSelectService;
+import edu.kh.cooof.member.model.dto.Member;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,25 +24,40 @@ public class TrainerSelectController {
 
 	private final TrainerSelectService service;
 	
+	// 트레이너 선택 페이지
 	@GetMapping("/trainerSelect")
 	public String trainerSelect(
-			Model model) {
+			Model model,
+			HttpSession session,
+			RedirectAttributes ra) {
 		
-		List<Trainer> trainers = service.getAllTrainers();	
-		model.addAttribute("trainers", trainers);
-	    return "gym/trainerSelect/trainerSelect";
+		Member loginMember = (Member) session.getAttribute("loginMember");
+		String path = null;	
+		
+		if(loginMember == null) {
+			ra.addFlashAttribute("message", "로그인 후 이용해 주세요.");
+			path = "/gym/gymMain";
+		} else {
+			List<Trainer> trainers = service.getAllTrainers();	
+			model.addAttribute("trainers", trainers);
+			path = "gym/trainerSelect/trainerSelect";
+		}
+		
+	    return path;
 	}
 	
 	@GetMapping("trainerPrice")
 	public String trainerPrice(
+			@RequestParam("price") int price,
 			Model model){
 		List<Trainer> trainers = service.getAllTrainers();
+		
+		model.addAttribute("trainerPrice", price);
 	    model.addAttribute("trainers", trainers);
 		return "gym/trainerSelect/trainerPrice";
 	}
 	
     
-	
 	
 	
 	
