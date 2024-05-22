5b5aad5aefdffca3b500e11d358407252680cc9e
inje <vkd2009@naver.com>
2024-05-22 17:34:24 +0900

    아직 db로 값이 가질 않음...ㅠ

--- UFC/src/main/java/edu/kh/cooof/gym/trainerSelect/controller/TrainerSelectController.java
+++ UFC/src/main/java/edu/kh/cooof/gym/trainerSelect/controller/TrainerSelectController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.cooof.gym.trainerSelect.model.dto.PaymentRequest;
@@ -17,17 +18,18 @@ import edu.kh.cooof.gym.trainerSelect.model.service.TrainerSelectService;
 import edu.kh.cooof.member.model.dto.Member;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("trainerSelect")
-
+@Slf4j
 public class TrainerSelectController {
 
 	private final TrainerSelectService service;
 	
 	// 트레이너 선택 페이지
-	@GetMapping("/trainerSelect")
+	@GetMapping("trainerSelect")
 	public String trainerSelect(
 			Model model,
 			HttpSession session,
@@ -54,27 +56,42 @@ public class TrainerSelectController {
 	public String trainerPrice(
 			@RequestParam("ptCount") int ptCount,
 			@RequestParam("trainerNo") int trainerNo,
+			HttpSession session,
 			Model model){
 		
-		Trainer trainer = service.selectTrainer(trainerNo);
+		Member loginMember = (Member) session.getAttribute("loginMember");
 		
-		model.addAttribute("trainer", trainer);
-		model.addAttribute("calcResult", trainer.getTrainerPrice() * ptCount);
 		
-		return "/gym/trainerSelect/trainerPrice"; // ${param.ptCount}
+		
+		if(loginMember != null) {
+			
+			 int memberNo = loginMember.getMemberNo(); 
+			 
+			 Trainer trainer = service.selectTrainer(trainerNo);
+			
+			model.addAttribute("trainer", trainer);
+			model.addAttribute("calcResult", trainer.getTrainerPrice() * ptCount);
+			model.addAttribute("memberNo", memberNo);
+
+			return "/gym/trainerSelect/trainerPrice"; // ${param.ptCount}
+		}
+		
+		return "/gym/trainerSelect/trainerSelect";
 	}
 	
-	@PostMapping("/gymPay")
-	public String gymPayment(@RequestBody PaymentRequest request) {
-		
-		
-		return service.gymPayment(request);
+	
+	
+	
+	@PostMapping("gymPay")
+	public String gymPayment(@RequestBody PaymentRequest request,                    
+	                         Model model) {
+	    
+	    return service.gymPayment(request);
+	    
 	}
 		
 	
     
-	
-	
 	
 	
 	
--- UFC/src/main/java/edu/kh/cooof/gym/trainerSelect/model/dto/PaymentRequest.java
+++ UFC/src/main/java/edu/kh/cooof/gym/trainerSelect/model/dto/PaymentRequest.java
@@ -15,4 +15,6 @@ public class PaymentRequest {
     private int ptPrice;
     private String strDate;
     private String ptLkroom;
+    private int memberNo;
+    private int trainerNo;
 }
