2416d040df4de5094db4609d5829d17f2b966e85
khs <hanseog201@naver.com>
2024-05-23 14:21:45 +0900

    목록으로 버튼 구현 완료

--- UFC/src/main/resources/static/js/lesson/lessonList/lessonDetail.js
+++ UFC/src/main/resources/static/js/lesson/lessonList/lessonDetail.js
@@ -1,4 +1,8 @@
+// 레슨 신청 버튼
 const signupBtn = document.querySelector("#signupBtn");
+
+// 목록으로 버튼
+const gobackBtn = document.querySelector("#gobackBtn");
 
 if (signupBtn != null) { // 신청 버튼 존재 시
     signupBtn.addEventListener("click", () => {
@@ -11,3 +15,22 @@ if (signupBtn != null) { // 신청 버튼 존재 시
 
     });
 }
+
+
+
+gobackBtn.addEventListener("click", () => {
+  // 목표 url 예시 http://localhost/lesson/list/search?query=&cp=1&order=desc&tags=19
+
+  // 쿼리스트링 얻어오기
+  const queryParams = window.location.search;  // '?query=&tags=19,20&cp=1&order=desc'
+
+  // http://localhost/lesson/list/search
+
+  // console.log('/lesson/list/search' + queryParams);
+  location.href = '/lesson/list/search' + queryParams;
+  
+
+
+
+  
+})
