d436aa2d30a5d1c23aef52153c4bcbd3880c234a
Jtaegyu <tgjeon1104@naver.com>
2024-05-24 16:35:04 +0900

    관리자 페이지 지원서 승인 거절 하는중

--- UFC/src/main/java/edu/kh/cooof/gym/management/controller/ManagementController.java
+++ UFC/src/main/java/edu/kh/cooof/gym/management/controller/ManagementController.java
@@ -1,20 +1,28 @@
 package edu.kh.cooof.gym.management.controller;
 
-import edu.kh.cooof.member.model.dto.Member;
-import edu.kh.cooof.gym.application.model.dto.Application;
-import edu.kh.cooof.gym.management.model.service.ManagementService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.kh.cooof.gym.application.model.dto.Application;
+import edu.kh.cooof.gym.management.model.service.ManagementService;
+import edu.kh.cooof.member.model.dto.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("management")
@@ -47,7 +55,7 @@ public class ManagementController {
 	    return "gym/management/applicationList";
 	}
 	
-	
+	// 지원서 상세 조회
 	@GetMapping("applicationDetail")
     public String appDetail(
     		@RequestParam("applicationNo") int applicationNo,
@@ -60,13 +68,35 @@ public class ManagementController {
 
         model.addAttribute("app", app);
         
-        log.info("Application: {}", app);
-        
         return "gym/management/applicationDetail";
     }
 	
 	
+	@GetMapping("/download/{applicationNo}")
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(@PathVariable int applicationNo) {
+		try {
+			Application app = service.getApplicationNo(applicationNo);
+			String resumePath = app.getResumePath();
+			Path filePath = Paths.get(resumePath).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+
+			if (!resource.exists()) {
+				return ResponseEntity.notFound().build();
+			}
+
+			return ResponseEntity.ok()
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+					.body(resource);
+		} catch (Exception e) {
+			return ResponseEntity.internalServerError().build();
+		}
+	}
+	
+	
+	
 }
+	
 
 
 
