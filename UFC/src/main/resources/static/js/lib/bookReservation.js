6138fd5650f8d48d7d772bd0e3b22c7a1809df01
SEUONG WOO <andymon@naver.com>
2024-06-03 15:00:41 +0900

    카테고리, 검색, 페이징 완료/ 예약 버그 수정/장바구니 페이지 삭제 버그 수정

--- UFC/src/main/resources/static/js/lib/bookReservation.js
+++ UFC/src/main/resources/static/js/lib/bookReservation.js
@@ -1,22 +1,28 @@
-const removeBtn = document.querySelectorAll(".remove-btn");
 const reserveCheck = document.querySelectorAll(".reserveCheck");
 const reserveBtn = document.querySelector("#reserveBtn");
 
-for(let i of removeBtn){
-    i.addEventListener("click", e => {
-        const value = i.previousElementSibling.innerText;
-        
-        console.log(value);
 
-        fetch("/reservation/delete?bookNo=" + value)
-        .then(resp => resp.text())
-        .then(html=>{
-            document.querySelector('.reservationBook').outerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error during fetch operation:', error);
+function bookReserveDelete(){
+
+    const removeBtn = document.querySelectorAll(".remove-btn");
+
+    for(let i of removeBtn){
+        i.addEventListener("click", e => {
+            const value = i.previousElementSibling.innerText;
+            
+            console.log(value);
+    
+            fetch("/reservation/delete?bookNo=" + value)
+            .then(resp => resp.text())
+            .then(html=>{
+                document.querySelector('.reservationBook').outerHTML = html;
+            })
+            .catch(error => {
+                console.error('Error during fetch operation:', error);
+            });
         });
-    });
+    }
+
 }
 
 function reserveBook(){
@@ -56,8 +62,3 @@ function reserveBook(){
     });
 
 }
-
-reserveBtn.addEventListener("click", e=>{
-    reserveBook();
-})
-
