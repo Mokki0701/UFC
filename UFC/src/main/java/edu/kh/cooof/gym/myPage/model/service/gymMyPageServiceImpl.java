3efc100a074255510cf9731becc51b02bf1be66c
Jtaegyu <tgjeon1104@naver.com>
2024-06-03 17:51:07 +0900

    헬스장 내 PT신청정보 조회 기능 완료

--- UFC/src/main/java/edu/kh/cooof/gym/myPage/model/service/gymMyPageServiceImpl.java
+++ UFC/src/main/java/edu/kh/cooof/gym/myPage/model/service/gymMyPageServiceImpl.java
@@ -2,6 +2,7 @@ package edu.kh.cooof.gym.myPage.model.service;
 
 import org.springframework.stereotype.Service;
 
+import edu.kh.cooof.gym.myPage.model.dto.GymMyPage;
 import edu.kh.cooof.gym.myPage.model.mapper.gymMyPageMapper;
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +12,9 @@ public class gymMyPageServiceImpl implements gymMyPageService{
 	
 	private final gymMyPageMapper mapper;
 
+	@Override
+	public GymMyPage getPTInfo(int memberNo) {
+
+		return mapper.selectPTInfo(memberNo);
+	}
 }
