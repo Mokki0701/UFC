143b6d2bd4915591f078199618d68801ceec23cb
Jtaegyu <tgjeon1104@naver.com>
2024-06-05 17:45:49 +0900

    헬스장 메인 페이지 3쪽 화면/기능 구현 , 헬스장 헤더 수정 조금

--- UFC/src/main/resources/static/js/gym/gymMain.js
+++ UFC/src/main/resources/static/js/gym/gymMain.js
@@ -1,26 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const modal = document.querySelector("#modal");
-    const modalTitle = document.querySelector("#modal-title");
-    const modalContent = document.querySelector("#modal-content");
-    const closeModal = document.querySelector(".close");
-  
-    document.querySelectorAll('.gym_events ul li, .gym_notices ul li').forEach(item => {
-      item.addEventListener('click', e => {
-        const title = e.target.getAttribute('data-title');
-        const content = e.target.getAttribute('data-content');
-        modalTitle.textContent = title;
-        modalContent.textContent = content;
-        modal.style.display = "block";
-      });
+  const modal = document.querySelector("#modal");
+  const modalTitle = document.querySelector("#modal-title");
+  const modalContent = document.querySelector("#modal-content");
+  const closeModal = document.querySelector(".close");
+
+  document.querySelectorAll('.gym_events ul li, .gym_notices ul li').forEach(item => {
+    item.addEventListener('click', event => {
+      const target = event.currentTarget;
+      const title = target.getAttribute('data-title');
+      const content = target.getAttribute('data-content');
+      modalTitle.textContent = title;
+      modalContent.textContent = content;
+      modal.style.display = "block";
     });
-  
-    closeModal.addEventListener('click', () => {
+  });
+
+  closeModal.addEventListener('click', () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener('click', event => {
+    if (event.target == modal) {
       modal.style.display = "none";
-    });
-  
-    window.addEventListener('click', e => {
-      if (e.target == modal) {
-        modal.style.display = "none";
-      }
-    });
+    }
+  });
 });
+
