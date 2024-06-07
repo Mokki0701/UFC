03832ecae80508629a9d29835cbf0744d278bbe0
kimare <kimarv@naver.com>
2024-06-07 14:16:13 +0900

    메인 커뮤니티 도서, 프로그램 탭 화면,기능구현

--- UFC/src/main/java/edu/kh/cooof/main/controller/RootController.java
+++ UFC/src/main/java/edu/kh/cooof/main/controller/RootController.java
@@ -1,16 +1,29 @@
 package edu.kh.cooof.main.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import edu.kh.cooof.lesson.dashBoard.dto.LessonListDTO;
+import edu.kh.cooof.lib.book.model.dto.Book;
+import edu.kh.cooof.main.model.sevice.MainService;
+import lombok.RequiredArgsConstructor;
+
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class RootController {
+	
+	private final MainService bookService;
+	
+	
 
 	@GetMapping("loginError")
 	public String loginError(RedirectAttributes ra) {
@@ -18,7 +31,26 @@ public class RootController {
 		return "redirect:/";
 	}
 	
+	//메인커뮤니티 탭 화면 출력
+	@GetMapping("")
+	public String communityLib(
+			Model model
+			) {
+		
+		//도서
+		List<Book> bookList = bookService.bookList();
+		//프로그램
+		List<LessonListDTO> lessonList = bookService.lessonList();
+		
+		model.addAttribute("bookList",bookList);
+		model.addAttribute("lessonList",lessonList);
+		
+		return "index";
+	}
 	
+	
+
+
 	
 	
 }
--- UFC/src/main/java/edu/kh/cooof/main/model/sevice/MainServiceimpl.java
+++ UFC/src/main/java/edu/kh/cooof/main/model/sevice/MainServiceimpl.java
@@ -1,9 +1,32 @@
 package edu.kh.cooof.main.model.sevice;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.cooof.lesson.dashBoard.dto.LessonListDTO;
+import edu.kh.cooof.lib.book.model.dto.Book;
+import edu.kh.cooof.main.mapper.MainPageMapper;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class MainServiceimpl implements MainService{
 
+	private final MainPageMapper mapper;
+	
+	//커뮤니티 탭 도서 리스트 출력
+	@Override
+		public List<Book> bookList() {
+			return mapper.bookList();
+		}
+	
+	//메인커뮤니트 탭 평생교육 프로그램 화면 
+	@Override
+	public List<LessonListDTO> lessonList() {
+		return mapper.lessonList();
+	}
 	
 }
