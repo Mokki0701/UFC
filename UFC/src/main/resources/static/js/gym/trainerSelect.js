6bc34a4fa5f43dc6c1cbf180bb35dcd371f9c4c4
Jtaegyu <tgjeon1104@naver.com>
2024-06-17 11:01:29 +0900

    헬스장 트레이너 선택페이지 에서 가격 선택시 오류 수정

--- UFC/src/main/resources/static/js/gym/trainerSelect.js
+++ UFC/src/main/resources/static/js/gym/trainerSelect.js
@@ -50,73 +50,41 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (endDate > currentDate) {
                     const formattedEndDate = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}`;
                     const confirmation = confirm(`${ptPrice.ptStrdate} 부터 ${formattedEndDate}까지 PT 진행 중입니다. 결제를 계속 하시겠습니까?`);
-                    if (confirmation) {
-                        const ptCount =  button.dataset.ptCount; // PT 횟수
-                        const memberGender = button.dataset.memberGender; // 이름
-                        
-                
-                        // form 태그 만들기
-                        const form = document.createElement("form");
-                        form.action = '/trainerSelect/trainerPrice';
-                        form.method = 'POST';
-                        
-                        // 횟수 입력된 input
-                        const input1 = document.createElement("input");
-                        input1.type = "hidden";
-                        input1.name = "ptCount";
-                        input1.value = ptCount;
-                        
-                        const input2 = document.createElement("input");
-                        input2.type = "hidden";
-                        input2.name = "trainerNo";
-                        input2.value = button.parentElement.parentElement.dataset.trainerNo;
-                        
-                        
-                        // form태그를 화면에 추가
-                        form.append(input1, input2);
-                        document.querySelector("body").append(form);
-                
-                        // form태그 제출
-                        form.submit();
-
-                       
-                    }else {
-                
+                    if (!confirmation) {
                         return;
                     }
                 }
-            } else {
-                const ptCount =  button.dataset.ptCount; // PT 횟수
-                const memberGender = button.dataset.memberGender; // 이름
-                
-        
-                // form 태그 만들기
-                const form = document.createElement("form");
-                form.action = '/trainerSelect/trainerPrice';
-                form.method = 'POST';
-                
-                // 횟수 입력된 input
-                const input1 = document.createElement("input");
-                input1.type = "hidden";
-                input1.name = "ptCount";
-                input1.value = ptCount;
-                
-                const input2 = document.createElement("input");
-                input2.type = "hidden";
-                input2.name = "trainerNo";
-                input2.value = button.parentElement.parentElement.dataset.trainerNo;
-                
-                
-                // form태그를 화면에 추가
-                form.append(input1, input2);
-                document.querySelector("body").append(form);
-        
-                // form태그 제출
-                form.submit();
             }
+
+            const ptCount = button.dataset.ptCount; // PT 횟수
+            const trainerNo = button.closest('.trainer-container').dataset.trainerNo; // 트레이너 번호
+
+            // form 태그 만들기
+            const form = document.createElement("form");
+            form.action = '/trainerSelect/trainerPrice';
+            form.method = 'POST';
+
+            // 횟수 입력된 input
+            const input1 = document.createElement("input");
+            input1.type = "hidden";
+            input1.name = "ptCount";
+            input1.value = ptCount;
+
+            const input2 = document.createElement("input");
+            input2.type = "hidden";
+            input2.name = "trainerNo";
+            input2.value = trainerNo;
+
+            // form태그를 화면에 추가
+            form.append(input1, input2);
+            document.querySelector("body").append(form);
+
+            // form태그 제출
+            form.submit();
         });
     });
 });
+
 
 
 
